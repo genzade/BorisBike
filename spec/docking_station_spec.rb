f220fab9d998a6bdc14bb5adbb2816ed89966041
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-23 18:14:56 +0000

    TDD first two user stories

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,6 +9,9 @@ describe DockingStation do
 
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
+    it 'releases a working bike' do
+      bike = docking_station.release_bike
+      expect(bike).to be_working
+    end
   end
-  
 end
