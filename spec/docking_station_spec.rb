570578ad8058559c1214eb1d62b355f5fecba563
Hamid Zade <hamidmamadzade@gmail.com>
2015-12-23 20:24:40 +0000

    instance variable "bike" created and tested

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -10,8 +10,26 @@ describe DockingStation do
   describe '#release_bike' do
     it { is_expected.to respond_to :release_bike }
     it 'releases a working bike' do
-      bike = docking_station.release_bike
+      bike = Bike.new
       expect(bike).to be_working
     end
+  end
+
+  describe '#dock' do
+    it { is_expected.to respond_to(:dock).with(1).argument }
+    it 'should dock' do
+      bike = Bike.new
+      expect(docking_station.dock(bike)).to eq bike
+    end
+
+    it 'returns docked bikes' do
+      bike = Bike.new
+      docking_station.dock(bike)
+      expect(docking_station.bike).to eq bike
+    end
+  end
+
+  describe '#bike' do
+    it { is_expected.to respond_to(:bike)}
   end
 end
